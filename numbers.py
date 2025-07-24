b9bab2961458ad02cd8525cbbc81be882e6f484c
Sakshi pawar2007 <sakshibornonseptember@gmail.com>
2025-07-24 17:05:47 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,15 @@
+a=5
+b=10
+c=30
+if a<b & a<c:
+   print("a is smallest")
+
+elif b<a & b<c:
+    print("b is smallest")
+    
+else:
+    print("c is smallest") 
+
+
+
+
---
+++ sspyton.py
@@ -0,0 +1,15 @@
+a=5
+b=10
+c=1
+if a<b & a<c:
+   print("a is smallest")
+
+elif b<a & b<c:
+    print("b is smallest")
+    
+else:
+    print("c is smallest")
+
+
+
+
